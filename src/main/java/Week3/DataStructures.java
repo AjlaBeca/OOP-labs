168a10064e75ba17f1cd7b072b7ca6b3a132e4ad
ajla <ajlabeca@gmail.com>
2023-10-22 21:37:33 +0200

    adding week 3 tasks

--- src/main/java/Week3/DataStructures.java
+++ src/main/java/Week3/DataStructures.java
@@ -3,17 +3,20 @@ package Week3;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 enum Status{
-    TO_DO, IN_PROGRESS, COMPLETED, CANCELLED;
+    TO_DO, IN_PROGRESS, COMPLETED, CANCELLED
 }
-public class TaskItem {
+class TaskItem {
     private int taskId;
-    private String textDescription;
+    private String taskDescription;
     private Status taskStatus;
     public TaskItem(int a, String b, Status c){
         taskId=a;
-        textDescription=b;
+        taskDescription=b;
         taskStatus=c;
     }
 
@@ -25,8 +28,8 @@ public class TaskItem {
         return taskStatus;
     }
 
-    public String getTextDescription() {
-        return textDescription;
+    public String getTaskDescription() {
+        return taskDescription;
     }
 }
 
@@ -40,12 +43,70 @@ public class DataStructures {
                 new TaskItem(3,"Go over tasks from lab2", Status.COMPLETED));
     }
 
-    // TODO create a method to get all objects
+    public List<TaskItem> getAllTasks() {
+        return tasks;
+    }
 
-    // TODO create a method getByStatus that will accept a Status parameter and filter the tasks with the provided status
+    //filters the tasks with the provided status
+    public List<TaskItem> getByStatus(Status status)
+    {
+        List<TaskItem> filteredTasks = new ArrayList<>();
+        for(TaskItem task : tasks){
+            if(task.getTaskStatus()==status){
+                filteredTasks.add(task);
+            }
+        }
+        return filteredTasks;
+    }
 
-    // TODO create a method to find tasks whose id parameter greater than or equal to 2
+    //finds tasks whose id parameter is greater than or equal to 2
+    public List<TaskItem> getTasks2 (){
+        List<TaskItem> filteredTasks2 = new ArrayList<>();
+        for (TaskItem task : tasks){
+            if(task.getTaskId()>=2){
+                filteredTasks2.add(task);
+            }
+        }
+        return filteredTasks2;
+    }
 
-    // TODO create a method that will, by using the forEach stream method, print to the console the description of each task
+    public void printDescription(){
+        tasks.forEach(task -> System.out.println(task.getTaskDescription()));
+    }
+
+    public static void main(String[] args) {
+        DataStructures taskManager = new DataStructures();
+
+
+        System.out.println("Tasks in progress:");
+        taskManager.getByStatus(Status.IN_PROGRESS).forEach(task -> System.out.println(task.getTaskDescription()));
+        System.out.println();
+
+        System.out.println("Tasks with ID >= 2:");
+        taskManager.getTasks2().forEach(task -> System.out.println(task.getTaskDescription()));
+        System.out.println();
+
+        System.out.println("Task Descriptions:");
+        taskManager.printDescription();
+    }
+
+    public class DbConnect {
+        private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/root";
+        private static final String USERNAME = "root";
+        private static final String PASSWORD = "";
+
+
+        private Connection connection = null;
+
+
+        public DbConnect() {
+            try {
+                connection = DriverManager.getConnection(CONNECTION_STRING, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 }
 
